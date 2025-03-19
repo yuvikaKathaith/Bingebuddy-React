557e5e052837020079e6de86cc013922f6f8adbf
Yuvika Kathaith <139555240+yuvikaKathaith@users.noreply.github.com>
2025-03-19 19:00:09 +0530

    Update validate.js

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -11,7 +11,7 @@ export const checkValidDataForSignUp = (email, password, name) => {
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
     const isNameValid = /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(name);
     if(!isEmailValid) return "Please enter a valid Email address.";
-    if(!isPasswordValid) return "Please enter a valid password of Minimum 8 characters, at least 1 letter & a number.";
+    if(!isPasswordValid) return "Please enter a valid password of Minimum 8 characters, a capital letter, a special character & a number.";
     if(!isNameValid) return "Name is not valid.";
     return null;
 } 
